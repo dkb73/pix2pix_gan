09fee6e8d73a493c7d48c246b5c757ffba12e993
DHWANAN BHARADVA <122605409+dkb73@users.noreply.github.com>
2024-11-15 21:32:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,16 +3,16 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-TRAIN_DIR = "data/train"
-VAL_DIR = "data/val"
+TRAIN_DIR = "data/data/train"
+VAL_DIR = "data/data/val"
 LEARNING_RATE = 2e-4
-BATCH_SIZE = 16
+BATCH_SIZE = 1
 NUM_WORKERS = 2
 IMAGE_SIZE = 256
 CHANNELS_IMG = 3
 L1_LAMBDA = 100
 LAMBDA_GP = 10
-NUM_EPOCHS = 500
+NUM_EPOCHS = 200
 LOAD_MODEL = False
 SAVE_MODEL = False
 CHECKPOINT_DISC = "disc.pth.tar"
@@ -24,8 +24,6 @@ both_transform = A.Compose(
 
 transform_only_input = A.Compose(
     [
-        A.HorizontalFlip(p=0.5),
-        A.ColorJitter(p=0.2),
         A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
         ToTensorV2(),
     ]
