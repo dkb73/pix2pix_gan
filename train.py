1aa169b9ea2395d16d1b8eef0ccaf43f655f8176
DHWANAN BHARADVA <122605409+dkb73@users.noreply.github.com>
2024-11-15 21:33:01 +0530

    Update train.py

--- train.py
+++ train.py
@@ -9,42 +9,55 @@ from discriminator_model import Discriminator
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from torchvision.utils import save_image
-
+from vgg_perceptual_loss import VGGPerceptualLoss
 torch.backends.cudnn.benchmark = True
 
+import os
+os.environ["PYTHONWARNINGS"] = "ignore"
+
+import warnings
+warnings.filterwarnings("ignore")
 
 def train_fn(
-    disc, gen, loader, opt_disc, opt_gen, l1_loss, bce, g_scaler, d_scaler,
+    disc, gen, loader, opt_disc, opt_gen, l1_loss, bce, vgg_loss, g_scaler, d_scaler,
 ):
     loop = tqdm(loader, leave=True)
 
     for idx, (x, y) in enumerate(loop):
         x = x.to(config.DEVICE)
         y = y.to(config.DEVICE)
-
+        # print(f"x {x.type()} {x.min()} {x.max()}")
+        # print(f"y {y.type()} {y.min()} {y.max()}")
         # Train Discriminator
-        with torch.cuda.amp.autocast():
-            y_fake = gen(x)
-            D_real = disc(x, y)
-            D_real_loss = bce(D_real, torch.ones_like(D_real))
-            D_fake = disc(x, y_fake.detach())
-            D_fake_loss = bce(D_fake, torch.zeros_like(D_fake))
-            D_loss = (D_real_loss + D_fake_loss) / 2
+
+        y_fake = gen(x)
+        # y_fake = y_fake.float()
+        D_real = disc(x, y)
+        D_real_loss = bce(D_real, torch.ones_like(D_real))
+        D_fake = disc(x, y_fake.detach())
+        D_fake_loss = bce(D_fake, torch.zeros_like(D_fake))
+        D_loss = (D_real_loss + D_fake_loss) / 2
 
         disc.zero_grad()
         d_scaler.scale(D_loss).backward()
+        d_scaler.unscale_(opt_disc)
+        # torch.nn.utils.clip_grad_norm_(disc.parameters(), max_norm=1.0)
         d_scaler.step(opt_disc)
         d_scaler.update()
 
         # Train generator
-        with torch.cuda.amp.autocast():
-            D_fake = disc(x, y_fake)
-            G_fake_loss = bce(D_fake, torch.ones_like(D_fake))
-            L1 = l1_loss(y_fake, y) * config.L1_LAMBDA
-            G_loss = G_fake_loss + L1
+        # print(f'y_fake {y_fake.type()}')
+        # print(f'y {y.type()}')
+        vgg_loss_1 = vgg_loss(y_fake, y)
+        D_fake = disc(x, y_fake)
+        G_fake_loss = bce(D_fake, torch.ones_like(D_fake))
+        L1 = l1_loss(y_fake, y) * config.L1_LAMBDA
+        G_loss = G_fake_loss + L1 + vgg_loss_1
 
         opt_gen.zero_grad()
         g_scaler.scale(G_loss).backward()
+        g_scaler.unscale_(opt_gen)
+        torch.nn.utils.clip_grad_norm_(gen.parameters(), max_norm=1.0)
         g_scaler.step(opt_gen)
         g_scaler.update()
 
@@ -52,16 +65,20 @@ def train_fn(
             loop.set_postfix(
                 D_real=torch.sigmoid(D_real).mean().item(),
                 D_fake=torch.sigmoid(D_fake).mean().item(),
+                vgg_loss_1=vgg_loss_1.item(),
+                L1=L1.item(),
+                G_fake_loss=G_fake_loss.item(),
             )
 
-
 def main():
+    print(f"device {config.DEVICE}")
     disc = Discriminator(in_channels=3).to(config.DEVICE)
     gen = Generator(in_channels=3, features=64).to(config.DEVICE)
     opt_disc = optim.Adam(disc.parameters(), lr=config.LEARNING_RATE, betas=(0.5, 0.999),)
     opt_gen = optim.Adam(gen.parameters(), lr=config.LEARNING_RATE, betas=(0.5, 0.999))
     BCE = nn.BCEWithLogitsLoss()
     L1_LOSS = nn.L1Loss()
+    VGG_LOSS = VGGPerceptualLoss().to(config.DEVICE)
 
     if config.LOAD_MODEL:
         load_checkpoint(
@@ -85,14 +102,14 @@ def main():
 
     for epoch in range(config.NUM_EPOCHS):
         train_fn(
-            disc, gen, train_loader, opt_disc, opt_gen, L1_LOSS, BCE, g_scaler, d_scaler,
+            disc, gen, train_loader, opt_disc, opt_gen, L1_LOSS, BCE,VGG_LOSS, g_scaler, d_scaler,
         )
 
         if config.SAVE_MODEL and epoch % 5 == 0:
             save_checkpoint(gen, opt_gen, filename=config.CHECKPOINT_GEN)
             save_checkpoint(disc, opt_disc, filename=config.CHECKPOINT_DISC)
 
-        save_some_examples(gen, val_loader, epoch, folder="evaluation")
+        save_some_examples(gen, val_loader, epoch, folder="data/")
 
 
 if __name__ == "__main__":
